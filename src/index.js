3c0524b6c1e5798e429de1dc25bdabbfb4001861
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-11-14 16:53:58 +0100

    creiamo un componente riusabile

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-function App(){
-    return <h1>Hello React!</h1>
+function App() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/margherita.jpg" alt="Pizza Margherita" />
+      <h2>Pizza Margherita</h2>
+      <p>Pizza with tomatoes and mozzarella</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
